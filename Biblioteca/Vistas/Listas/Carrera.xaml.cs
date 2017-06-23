5eb19ba1de667c4b092151861840e453e4810853
Jorge <jorgeluis.garcia@ujcv.edu.hn>
2017-06-23 10:05:23 -0600

    Establecer estructura maestro-detalle

--- Biblioteca/Vistas/Listas/Carrera.xaml.cs
+++ Biblioteca/Vistas/Listas/Carrera.xaml.cs
@@ -20,9 +20,9 @@ namespace Biblioteca
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Lista : Page
+    public sealed partial class Carrera : Page
     {
-        public Lista()
+        public Carrera()
         {
             this.InitializeComponent();
         }
--- Biblioteca/Vistas/Maestro.xaml.cs
+++ Biblioteca/Vistas/Maestro.xaml.cs
@@ -20,9 +20,9 @@ namespace Biblioteca
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Carreras : Page
+    public sealed partial class Maestro : Page
     {
-        public Carreras()
+        public Maestro()
         {
             this.InitializeComponent();
         }
